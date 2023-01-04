4226b3bdb99c371fba20af67ea28a403fec546e6
Alberto Ortiz Flores <aortiz@nyu.edu>
2023-01-04 11:22:17 -0500

    Updates

--- templates/epubs.tpl.php
+++ templates/epubs.tpl.php
@@ -1,21 +1,14 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+<nav class="breadcrumb" aria-label="breadcrumbs">
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/epubs">ePubs</a></li>
+    <li><a href="/epubs/nyupress">New York University Press</a></li>
+    <li class="is-active"><a aria-current="page"><?php print $title ?></a></li>
+  </ul>
+</nav>  
 <div class="content">
   <article class="content">
-    <h5><a href="/admin/epubs">Epubs</a> - <a href="/admin/epubs/nyupress">New York University Press</a> - <?php print $title ?></h5>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal" style="text-align: left;">
-        <label class="label">Choose version</label>
-      </div>
-      <div class="field-body">
-        <div class="select">
-          <select name="versions" id="versions">
-            <?php foreach ($versions as $version): ?>
-              <?php echo "<option value=$version[hash]>$version[time]</option>" ?>
-            <?php endforeach; ?>
-          </select>
-        </div>
-      </div>
-    </div>
     <?php print $metadata ?>
   </article>
 </div>
