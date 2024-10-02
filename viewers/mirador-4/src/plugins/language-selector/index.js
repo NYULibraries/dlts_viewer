459956ad6cbd2eb171b7610e10bd07acdd729baa
Damon Chu <dc3800@nyu.edu>
2024-10-02 12:28:16 -0400

    Style changes for Mirador 4

--- viewers/mirador-4/src/plugins/language-selector/index.js
+++ viewers/mirador-4/src/plugins/language-selector/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
-import { FormControl } from '@material-ui/core'
-import CheckIcon from '@material-ui/icons/CheckSharp'
-import MenuItem from '@material-ui/core/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import { FormControl } from '@mui/material'
+import CheckIcon from '@mui/icons-material/CheckSharp'
+import MenuItem from '@mui/material/MenuItem'
 import CollapsibleSection from 'mirador/dist/es/src/containers/CollapsibleSection'
 import { updateConfig } from 'mirador/dist/es/src/state/actions/config'
 import { getLanguagesFromConfigWithCurrent } from 'mirador/dist/es/src/state/selectors/config'
@@ -13,11 +13,12 @@ import translations from './translations'
 
 const langstyles = {
   container: {
+    width: '100%',
     marginTop: '16px',
-    paddingTop: '16px',
-    paddingLeft: '0',
-    paddingRight: '8px',
-    paddingBottom: '8px',
+    paddingBlockStart: '16px',
+    paddingInlineStart: '0',
+    paddingInlineEnd: '8px',
+    paddingBlockEnd: '8px',
     borderBottom: 'none',
     borderTop: '0.5px solid rgba(0, 0, 0, 0.25)',
   },
@@ -26,16 +27,30 @@ const langstyles = {
     justifyContent: 'space-around',
     alignItems: 'start',
     flexDirection: 'column',
-    margin: '.5em 0',
+    margin: '0',
   },
   formControl: {
     width: '100%',
+    marginTop: '0', 
+  },
+  sectionTitle: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingRight: '8px', 
   },
   selectMenu: {
     '&::before': {
       content: "''",
       borderBottom: '.1px solid rgba(0, 0, 0, 0.2)',
     },
+  },
+  listItemText: {
+    paddingInlineStart: '20px',
+    paddingInlineEnd: '20px',
+    marginBlockStart: '4px',
+    marginBlockEnd: '4px',
   },
 }
 
@@ -68,7 +83,10 @@ const LanguageSelector = (props) => {
                     }}
                   >
                     <ListItemIcon>{language.current && <CheckIcon />}</ListItemIcon>
-                    <ListItemText primaryTypographyProps={{ variant: 'body1' }}>
+                    <ListItemText 
+                      primaryTypographyProps={{ variant: 'body1' }}
+                      style={langstyles.listItemText}
+                    >
                       {language.label}
                     </ListItemText>
                   </MenuItem>
@@ -80,7 +98,6 @@ const LanguageSelector = (props) => {
       </CollapsibleSection>
     </div>
   )
-
 }
 
 const mapDispatchToProps = (dispatch, { afterSelect }) => ({
