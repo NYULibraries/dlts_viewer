af01d544bdda2fb4c7735155d37c4275d1c85fcd
Alberto Ortiz Flores <aortiz@nyu.edu>
2026-05-18 09:31:44 -0400

    serials

---
+++ templates/embed_mirador_ext.tpl.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="<?php echo $language ?>">
+  <head>
+    <meta charset="utf-8" />
+    <title><?php echo $title ?></title>
+    <?php if (isset($analytics) && $analytics['enabled']): ?>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $analytics['code'] ?>"></script>
+      <script>
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+        gtag('config', '<?php echo $analytics['code'] ?>')
+      </script>
+    <?php endif; ?>
+    <link rel="stylesheet" href="<?php echo $css_url ?>">
+  </head>
+  <body>
+    <div
+      id="<?php echo $appid ?>"
+      dir="<?php echo $direction ?>"
+      data-language="<?php echo $language ?>"
+      data-endpoint="<?php echo $endpoint ?>"
+      data-type="<?php echo $type ?>"
+      data-sequence="<?php echo $sequence ?>"
+      data-identifier="<?php echo $identifier ?>"
+      data-show-canvas-info="<?php echo $show_canvas_info ?>"
+      data-show-collection="<?php echo $show_collection ?>"
+      data-multivolume="<?php echo $is_multivolume ?>"
+    >
+    </div>
+    <script type="module" src="<?php echo $js_url ?>"></script>
+  </body>
+</html>
